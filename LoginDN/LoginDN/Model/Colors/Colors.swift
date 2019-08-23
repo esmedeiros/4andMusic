f4f8ebf2f06d1882ca10017e61dafec06a813564
Alexandre Aun <alexandreaun@gmail.com>
2019-08-22 22:04:21 -0300

    classe de colors criada

---
+++ LoginDN/LoginDN/Model/Colors/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  LoginDN
+//
+//  Created by Alexandre Aun on 22/08/19.
+//  Copyright © 2019 Mac Pro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Colors{
+    
+    let orange: UIColor = UIColor(red: 192/255, green: 51/255, blue: 98/255, alpha: 1.0)
+    let pink: UIColor = UIColor(red: 205/255, green: 99/255, blue: 69/255, alpha: 1.0)
+    
+}
